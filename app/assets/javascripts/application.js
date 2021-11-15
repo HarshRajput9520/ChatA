553eb4ace5bd69468b6c866ef87f83b813bd5444
HarshRajput9520 <rajputharsh7999@gmail.com>
2021-11-15 17:14:37 +0530

    First Commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require jquery.min
+//= require bootstrap.bundle.min
+//= require simplebar.min
+//= require waves.min
+//= require jquery.magnific-popup.min
+//= require owl.carousel.min
+//= require index.init
+//= require app
